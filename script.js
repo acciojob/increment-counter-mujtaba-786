7acf981747a4a1fc727c0f2d63eb10ec911bf01f
MOHD MUJTABA <56441628+mujtaba-786@users.noreply.github.com>
2025-10-29 16:49:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+let btn = document.getElementById("btn")
+let p = document.querySelector("p")
+let counter = 0
+btn.addEventListener('click', function() {
+     counter+= 1
+     p.innerHTML = counter
+})
