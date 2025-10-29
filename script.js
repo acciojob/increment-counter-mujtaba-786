2cc16017c6af60d79d4dbf703ae288b4da839d3a
MOHD MUJTABA <56441628+mujtaba-786@users.noreply.github.com>
2025-10-29 16:59:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //your JS code here. If required.
 
-let btn = document.getElementById("btn")
-let p = document.querySelector("p")
+let btn = document.getElementById("incrementBtn")
+let p = document.querySelector("#counter")
 let counter = 0
 btn.addEventListener('click', function() {
-	alert(counter)
-     counter+= 1
-     p.innerHTML = counter
+     alert(counter)
+     counter+= 1 
+     p.textContent = counter 
 })
